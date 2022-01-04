646cc6ad9c41c4ee965ae77d45a6850d5de6c832
Gledyson <rgleydson794@gmail.com>
2022-01-04 18:28:43 -0300

    exercicios 2

---
+++ js/exercicios1.js
@@ -0,0 +1,25 @@
+// define variable
+const weight = 63.5
+
+// type data of variable
+console.log(typeof weight)
+
+// create object
+const person = {
+    name: "João",
+    age: 20,
+    isHuman: true,
+    weight: 1.80
+}
+
+console.log(`He is human? ${person.isHuman}`)
+
+// learn function
+
+function seeFunction(phrase){
+    return phrase
+}
+
+const test = seeFunction('deu certo')
+
+console.log(test)
